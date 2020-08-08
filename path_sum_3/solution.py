890f4244d65d9602ebf64d07053651aedb10e35f
Hossein Zeinali <hosseinzeinalii@gmail.com>
2020-08-09 00:35:33 +0430

    Fix path sum 3 by dfs.

--- path_sum_3/solution.py
+++ path_sum_3/solution.py
@@ -7,23 +7,22 @@ class TreeNode:
 
 class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> int:
-        res = []
+        if root == None:
+            return 0
+        res = [0]
         self.helper(root,sum, [], res)
 
-        return res
+        return res[0]
     def helper(self, root: TreeNode, sum, numsArray: [], res: []):
         numsArray.append(root.val)
         i = len(numsArray) - 1
         s = 0
         sumArray = []
         while i >= 0:
-            s+= numsArray[i]
-            if (s > sum):
-                break
+            s += numsArray[i]
             sumArray.append(numsArray[i])
             if (s == sum):
-                res.append(list(reversed(sumArray)))
-
+                res[0] = res[0] + 1
             i = i -1
         if root.left != None:
             self.helper(root.left, sum, numsArray.copy(), res)
@@ -33,27 +32,32 @@ class Solution:
 
 
     def makeTree(self):
-        root = TreeNode(10)
-        node5 = TreeNode(5)
-        nodem3 = TreeNode(-3)
-        node3 = TreeNode(3)
-        node2 = TreeNode(2)
-        node11 = TreeNode(11)
-        node3p = TreeNode(3)
-        nodem2 = TreeNode(-2)
+        # root = TreeNode(10)
+        # node5 = TreeNode(5)
+        # nodem3 = TreeNode(-3)
+        # node3 = TreeNode(3)
+        # node2 = TreeNode(2)
+        # node11 = TreeNode(11)
+        # node3p = TreeNode(3)
+        # nodem2 = TreeNode(-2)
+        # node1 = TreeNode(1)
+        # root.left = node5
+        # root.right = nodem3
+        # node5.left = node3
+        # node5.right = node2
+        # node3.left = node3p
+        # node3.right = nodem2
+        # node2.right = node1
+        # nodem3.right = node11
+        root = TreeNode(0)
         node1 = TreeNode(1)
-        root.left = node5
-        root.right = nodem3
-        node5.left = node3
-        node5.right = node2
-        node3.left = node3p
-        node3.right = nodem2
-        node2.right = node1
-        nodem3.right = node11
+        node1p = TreeNode(1)
+        root.left = node1
+        root.right = node1p
 
         return root
 
 solution = Solution()
 root = solution.makeTree()
-a = solution.pathSum(root, 8)
+a = solution.pathSum(root, 1)
 print(a)
